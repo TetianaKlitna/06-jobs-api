202cbf292ecb18ee8508e2a1b925b5de750a2954
Tetiana Klitna <klitna.tetiana@gmail.com>
2025-09-05 10:32:54 -0700

    describe errors

--- errors/custom-api.js
+++ errors/custom-api.js
@@ -1,7 +1,10 @@
+const { StatusCodes } = require('http-status-codes');
 class CustomAPIError extends Error {
-  constructor(message) {
-    super(message)
+  constructor(message, statusCode = StatusCodes.INTERNAL_SERVER_ERROR) {
+    super(message);
+    this.statusCode = statusCode;
+    Error.captureStackTrace(this, this.constructor);
   }
 }
 
-module.exports = CustomAPIError
+module.exports = CustomAPIError;
--- middleware/authentication.js
+++ middleware/authentication.js
@@ -9,13 +9,14 @@ const auth = async (req, res, next) => {
   }
   const token = authHeader.split(' ')[1];
   if (!token) {
-    throw new UnauthenticatedError('Please provide Authorization token');
+    throw new UnauthenticatedError('Authentication invalid');
   }
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
     req.user = { userId: payload.userId, name: payload.name };
     next();
   } catch (error) {
+    console.error(error);
     throw new UnauthenticatedError('Authentication invalid');
   }
 };
